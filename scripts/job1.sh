de5f6f222855990002d9674b7fcc86e5fa6c9a66
Stefano <64137778+stedc1976@users.noreply.github.com>
2020-11-23 12:35:38 +0100

    Update job1.sh

--- scripts/job1.sh
+++ scripts/job1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo '{"labels": {"env": "dev"}, "results": {"items": 21} }'
+echo 'log_file_raw_count{pod_name="samigrationpam-kieserver-26-build",namespace="service-activator",container_name="sti-build"} 3596 1605829267522'
 
 exit 0
